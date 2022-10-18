59affe02cf91bbdef6b748912653bb73afb604a0
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-10-18 10:29:39 -0500

    k

--- src/main/java/futbol/Futbolista2.java
+++ src/main/java/futbol/Futbolista2.java
@@ -1,7 +1,7 @@
 package futbol;
 import java.util.ArrayList;
 
-public class Futbolista implements Comparable<Futbolista>,nece {
+public class Futbolista2 implements nece {
 	private String nombre;
 	private int edad;
 	private final String posicion;
